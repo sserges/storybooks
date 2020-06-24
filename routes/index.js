a33b27c3416b2fea477c403e2bbd517216fafb6e
sserges <agounkeserge@gmail.com>
2020-06-24 09:48:56 +0000

    Dashboard Stories

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { ensureAuth, ensureGuest } = require('../middlewares/auth');
+const Story = require('../models/Story');
 
 // @desc Login/Landing page
 // @route GET /
@@ -13,10 +14,17 @@ router.get('/', ensureGuest, (req, res) => {
 
 // @desc Dashboard
 // @route GET /dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-  res.render('dashboard', {
-    name: req.user.firstName,
-  });
+router.get('/dashboard', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ use: req.user.id }).lean();
+    res.render('dashboard', {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (error) {
+    console.error(error);
+    res.render('errors/500');
+  }
 });
 
 module.exports = router;
