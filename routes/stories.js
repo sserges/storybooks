f269239919fd01ec26fc502f040db2de48592bc1
sserges <agounkeserge@gmail.com>
2020-06-25 13:10:44 +0000

    Method Override For PUT Requests

--- routes/stories.js
+++ routes/stories.js
@@ -63,4 +63,30 @@ router.get('/edit/:id', ensureAuth, async (req, res) => {
   }
 });
 
+// @desc Update story
+// @route PUT /stories/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean();
+
+    if (!story) {
+      return res.render('errors/404');
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect('/stories');
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+
+      res.redirect('/dashboard');
+    }
+  } catch (error) {
+    console.error(error);
+    res.render('errors/500');
+  }
+});
+
 module.exports = router;
