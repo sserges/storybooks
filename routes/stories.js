3f0778a083a786dfae8b041a21b6fff8ef4ca82f
sserges <agounkeserge@gmail.com>
2020-06-26 00:04:33 +0000

    Single Story Page

--- routes/stories.js
+++ routes/stories.js
@@ -20,6 +20,25 @@ router.get('/', ensureAuth, async (req, res) => {
   }
 });
 
+// @desc Show single story
+// @route GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate('user').lean();
+
+    if (!story) {
+      return res.render('errors/404');
+    }
+
+    res.render('stories/show', {
+      story,
+    });
+  } catch (error) {
+    console.error(error);
+    res.render('errors/500');
+  }
+});
+
 // @desc Process add form
 // @route POST /stories
 router.post('/', ensureAuth, async (req, res) => {
