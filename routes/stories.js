f5b45f724ee501bf295c7aff441e5154aa23f83f
sserges <agounkeserge@gmail.com>
2020-06-26 00:35:05 +0000

    User Stories

--- routes/stories.js
+++ routes/stories.js
@@ -120,4 +120,24 @@ router.delete('/:id', ensureAuth, async (req, res) => {
   }
 });
 
+// @desc User stories
+// @route GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: 'public',
+    })
+      .populate('user')
+      .lean();
+
+    res.render('stories/index', {
+      stories,
+    });
+  } catch (error) {
+    console.error(error);
+    res.render('errors/500');
+  }
+});
+
 module.exports = router;
